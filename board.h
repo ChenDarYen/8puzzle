ec24af58f9539a0c0b02db6f4819d871ca04edf0
Chen Dar-Yen <chendaryen@outlook.com>
2019-12-26 17:13:52 +0800

    RBFS

--- board.h
+++ board.h
@@ -35,17 +35,16 @@ class Node
   friend class Board;
 public:
   using state = std::vector<int>;
-  Node() : s(0), g(0), h(0) {}
+  Node() : s(0), g(0), h(0), f(0) {}
   Node(const state &state, int x, int y, std::shared_ptr<Node> n = nullptr) :
-      s(state), g(x), h(y), prev(n) {}
+      s(state), g(x), h(y), f(x+y), prev(n) {}
   Node(const state::iterator b, const state::iterator e, int x, int y, std::shared_ptr<Node> n = nullptr) :
-      s(b, e), g(x), h(y), prev(n) {}
+      s(b, e), g(x), h(y), f(x+y), prev(n) {}
 
 private:
   state s;
-  int g, h;
+  int g, h, f;
   std::shared_ptr<Node> prev;
-  int f() const { return g + h; }
 };
 
 class Board
@@ -55,7 +54,7 @@ class Board
 public:
   using state = Node::state;
   using action = int;
-  Board(state s) : root(std::make_shared<Node>(s, 0, 0)) {}
+  Board(state s) : root(std::make_shared<Node>(s, 0, heuristic(s))) {}
   void search();
   void print(std::ostream&);
   const state& getInit() const { return root->s; }
@@ -63,14 +62,14 @@ public:
 private:
   std::shared_ptr<Node> root;
   std::shared_ptr<Node> curr;
-  std::vector<std::shared_ptr<Node>> frontier;
-  void expandCurr();
-  state result(const state&, int);
-  std::vector<action> actions(const state&);
-  bool goalTest(const state&);
-  int heuristic(const state&);
-  size_t findNum(size_t, const state &);
-  void printSate(std::ostream &, const state&);
+  void expand(std::shared_ptr<Node>, std::vector<std::shared_ptr<Node>>&) const;
+  state result(const state&, int) const;
+  std::vector<action> actions(const state&) const;
+  bool goalTest(const state&) const;
+  int heuristic(const state&) const;
+  size_t findNum(size_t, const state &) const;
+  void printSate(std::ostream &, const state&) const;
+  std::pair<std::shared_ptr<Node>, int> RBFS(std::shared_ptr<Node>, int) const;
 };
 
 // friend
@@ -91,17 +90,8 @@ void Board::search()
 {
   if (!curr)
   {
-    if (isSovable(*this))
-    {
-      curr = root;
-      while(!goalTest(curr->s))
-      {
-        expandCurr();
-        std::pop_heap(frontier.begin(), frontier.end());
-        curr = frontier.back();
-        frontier.pop_back();
-      }
-    }
+    if(isSovable(root->s))
+      curr = RBFS(root, INFTY).first;
     else
       curr = std::make_shared<Node>();
   }
@@ -133,17 +123,17 @@ void Board::print(std::ostream &os)
 }
 
 //private
-void Board::expandCurr()
+void Board::expand(std::shared_ptr<Node> n, std::vector<std::shared_ptr<Node>> &v) const
 {
-  for (auto a : actions(curr->s))
+  for (auto a : actions(n->s))
   {
-    state newS = result(curr->s, a);
-    frontier.push_back(std::make_shared<Node>(newS, curr->g + 1, heuristic(newS), curr));
-    std::push_heap(frontier.begin(), frontier.end(),
-                   [](auto a, auto b) { return a->f() > b->f(); });
+    state newS = result(n->s, a);
+    v.push_back(std::make_shared<Node>(newS, n->g + 1, heuristic(newS), n));
+    std::push_heap(v.begin(), v.end(),
+                   [](auto a, auto b) { return a->f > b->f; });
   }
 }
-std::vector<Board::action> Board::actions(const state &s)
+std::vector<Board::action> Board::actions(const state &s) const
 {
   std::vector<action> ret;
   int pos_blank(findNum(0, s));
@@ -157,13 +147,13 @@ std::vector<Board::action> Board::actions(const state &s)
     ret.push_back(3);
   return ret;
 }
-size_t Board::findNum(size_t n, const state &s)
+size_t Board::findNum(size_t n, const state &s) const
 {
   for (size_t i = 0; i < 9; ++i)
     if (s[i] == n) return i;
   return -1;
 }
-int Board::heuristic(const state &s)
+int Board::heuristic(const state &s) const
 {
   int manhattan_dist(0);
   for (size_t i = 1; i < 9; ++i)
@@ -173,7 +163,7 @@ int Board::heuristic(const state &s)
   }
   return manhattan_dist;
 }
-Board::state Board::result(const state &s, int direction)
+Board::state Board::result(const state &s, int direction) const
 {
   state trans_state(s);
   size_t pos_swap, pos_balnk = findNum(0, trans_state);
@@ -188,14 +178,14 @@ Board::state Board::result(const state &s, int direction)
   std::swap(trans_state[pos_balnk], trans_state[pos_swap]);
   return trans_state;
 }
-bool Board::goalTest(const state &s)
+bool Board::goalTest(const state &s) const
 {
   for (size_t i = 1; i < 9; i ++)
     if (findNum(i, s) != i-1)
       return false;
   return true;
 }
-void Board::printSate(std::ostream &os, const state &s)
+void Board::printSate(std::ostream &os, const state &s) const
 {
   for (size_t i = 0; i < 9; ++i)
   {
@@ -204,5 +194,37 @@ void Board::printSate(std::ostream &os, const state &s)
     else 
       os << std::endl;
     os << s[i];
+  }
+}
+std::pair<std::shared_ptr<Node>, int> Board::RBFS(std::shared_ptr<Node> n, int f_limit) const
+{
+  if (goalTest(n->s))
+    return std::make_pair(n, 0);
+  std::vector<std::shared_ptr<Node>> successors;
+  expand(n, successors);
+  if(successors.empty())
+    return std::make_pair(nullptr, INFTY);
+  for (auto s : successors)
+    s->f = std::max(s->f, n->f);
+  
+  while(true)
+  {
+    std::pop_heap(successors.begin(), successors.end());
+    auto best(successors.back());
+    successors.pop_back();
+    if(best->f > f_limit)
+      return std::make_pair(nullptr, INFTY);
+
+    std::pop_heap(successors.begin(), successors.end());
+    int alt(successors.back()->f);
+    std::push_heap(successors.begin(), successors.end());
+
+    auto p(RBFS(best, std::min(f_limit, alt)));
+    if (p.first)
+      return std::make_pair(p.first, 0);
+    best->f = p.second;
+
+    successors.push_back(best);
+    std::push_heap(successors.begin(), successors.end());
   }
 }
